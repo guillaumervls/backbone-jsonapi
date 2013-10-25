b92f0b40d6622d622ff58f951181f695d2b205af
guillaumervls <guillaumervls@server.fake>
2013-10-25 17:25:04 +0200

    Fixed UMD

--- src/umd.js
+++ src/umd.js
@@ -1,10 +1,10 @@
 /*global define*/
-module.exports = require('./backbone-jsonapi');
+var backboneJsonApi = require('./backbone-jsonapi');
 if (typeof define === 'function' && define.amd) {
   define(['Backbone', '_'], function (Backbone, _) {
-    window.backboneJsonApi = module.exports(Backbone, _);
+    window.backboneJsonApi = backboneJsonApi(Backbone, _);
     return window.backboneJsonApi;
   });
 } else {
-  window.backboneJsonApi = module.exports(window.Backbone, window._);
+  window.backboneJsonApi = backboneJsonApi(window.Backbone, window._);
 }
